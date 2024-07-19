c4e5733e267d1a2f7a2eeedb66fc9bbb8fedcb89
palmeros <pedrofpalmeros@gmail.com>
2024-07-18 21:30:18 -0600

    Adding example of just reading a char

---
+++ CPP_21_Challenge/12_Arrays_C_strings_linked_lists/08_fill_array/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+    char buffer[80];
+
+    cout << "Write the chain: ";
+    cin >> buffer;
+    cout << "Here is the buffer: " << buffer << endl; 
+}
